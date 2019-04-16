17e429abb74870fa49be716d77bfc4a22a00b4a1
Marcello Alves <marcello_doalves@hotmail.com>
2019-04-16 11:06:18 -0300

    Configurando ambiente

---
+++ HelloTypeScript/main.js
@@ -0,0 +1,6 @@
+var tent = 'Hello World';
+function minhaFunction(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
---
+++ HelloTypeScript/main.ts
@@ -0,0 +1,9 @@
+var tent = 'Hello World'
+
+function minhaFunction(x, y) {
+    return x + y;
+}
+
+
+let num = 2;
+const PI = 3.14;

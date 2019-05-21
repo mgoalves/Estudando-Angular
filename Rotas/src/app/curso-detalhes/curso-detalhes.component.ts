0734b0e40f20091492b435c0248677072c2e7c76
Marcello Alves <marcello_doalves@hotmail.com>
2019-05-21 17:01:22 -0300

    Aula #54-LG

--- Rotas/src/app/curso-detalhes/curso-detalhes.component.ts
+++ Rotas/src/app/curso-detalhes/curso-detalhes.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-curso-detalhes',
-  templateUrl: './curso-detalhes.component.html',
-  styleUrls: ['./curso-detalhes.component.css']
+	selector: 'app-curso-detalhes',
+	templateUrl: './curso-detalhes.component.html',
+	styleUrls: ['./curso-detalhes.component.css']
 })
 export class CursoDetalhesComponent implements OnInit {
 
-  id: number;
+	id: number;
+	subscriptionActive: Subscription;
 
-  constructor(private activatedRoute: ActivatedRoute) {
-    this.id = this.activatedRoute.snapshot.params['id'];
-   }
+	constructor(private activatedRoute: ActivatedRoute) {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
 
+		this.subscriptionActive = this.activatedRoute.params.subscribe(
+			(params) => { this.id = params['id'] }
+		);
+	}
+
+	ngOnDestroy(): void {
+
+		this.subscriptionActive.unsubscribe();
+	}
 }

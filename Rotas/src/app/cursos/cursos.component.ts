1b40c25ff0423f00d1f3df463003313fe462681b
Marcello Alves <marcello_doalves@hotmail.com>
2019-05-26 10:15:08 -0300

    Aula #56-LG

--- Rotas/src/app/cursos/cursos.component.ts
+++ Rotas/src/app/cursos/cursos.component.ts
@@ -1,5 +1,8 @@
-import { CursosService } from './cursos.service';
+import { Subscription } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CursosService } from './cursos.service';
+import { query } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-cursos',
@@ -9,13 +12,28 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   cursos = [];
+  pagina: number;
+  insActRoute: Subscription;
 
-  constructor(private cursosService: CursosService) {
+  constructor(private cursosService: CursosService, private activedRoute: ActivatedRoute, private router: Router) {
 
   }
 
   ngOnInit() {
-
     this.cursos = this.cursosService.getCursos();
+    this.insActRoute = this.activedRoute.queryParams.subscribe(
+
+      (query) => {
+        this.pagina = query['pagina'];
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.insActRoute.unsubscribe();
+  }
+
+  proximaPagina(): void {
+    this.pagina++;
+    this.router.navigate(['/cursos'], { 'queryParams': { 'pagina': this.pagina } });
   }
 }

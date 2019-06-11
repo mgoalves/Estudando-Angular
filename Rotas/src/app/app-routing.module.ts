47dce4e12d84cfc7a9c8123500dbc08c4cb47ea4
Marcello Alves <marcello_doalves@hotmail.com>
2019-06-11 17:26:34 -0300

    Aula #70-LG

--- Rotas/src/app/app-routing.module.ts
+++ Rotas/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { AuthGuard } from './guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -7,8 +8,10 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: 'alunos', loadChildren: './aluno/alunos.module#AlunosModule', canActivate: [AuthGuard], canLoad: [AuthGuard] },
   { path: 'cursos', loadChildren: './curso/cursos.module#CursosModule', canActivate: [AuthGuard], canLoad: [AuthGuard] },
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: '**', component: PaginaNaoEncontradaComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
